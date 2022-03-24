7d967843b53d8fa0e31e10c5217bb5407cee9218
Jonelle Villar <jonellevillar@gmail.com>
2022-03-24 21:47:15 +0100

    glycosylation

---
+++ Notes_glycosylation.R
@@ -0,0 +1,14 @@
+Clinical presentations are heterogeneous in both severity and symptoms, but most individuals experience significant impariment in functioning due to the lifetime burden of cofnitive problems (executive and memory impairment)l social withdrawl (avolition, anhedonia), and psychosis (hallucinations and delusion).  (Owen, 2016, Kahn, 2015) 
+
+Nearly all of our current treatments are based on decades-old hypotheses and the serendipitous discovery that sedative antihistamines blocking dopamine and serotonin systems provide some symptomatic relief (Mealer, 2020)
+
+The strongest coding variant in SCZ GWAS is a missense mutation in the manganese transporter SLC39A8, which is associated with altered glycosylation patterns in humans (Mealer, 2020)
+
+Translating findings to mechanistic and therpeutic insidghts remains a challenge, particiularly given that most common variant risk loci  (from GWAS) exict in the noncoding /regulatory region of the fenome. 
+
+Glycosulation is a fundamental biological process - seen in nearly all cellular pathways and most disease states
+Involes the Complex and highly regulated activity of enzymes and chaperones that coompreise nearly 2% of all proteins coded in the human genome. 
+
+THese enzymes geneate a diverse set of glycans ranging from single monosaccharides to oligosaccharides and polysaccarides covalently attached to proteins and lipids to regulated their structure, function, and localization 
+
+Glycosyltransferase gene families usually contain one or a few isoenzymes capable of catalyzing the the same step of glycan synthesis. 
