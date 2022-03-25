d7e1f2445561101adc8630282bb975b8f39f1ef7
Jonelle Villar <jonellevillar@gmail.com>
2022-03-25 22:15:05 +0100

    outline and slides

---
+++ extraSlides.R
@@ -0,0 +1,8 @@
+class:top left
+### Cell-type populations are influenced by:
+
+- AP and polypharmacy <br> <br />
+  - Gender, age, smoking, diet <br> <br />
+  - Diagnosis & health status <br> <br />
+  - Time of day & season <br> <br />
+  
