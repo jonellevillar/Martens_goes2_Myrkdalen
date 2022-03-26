ed27e997c0ce2c64b59fcbc9e4f588c751cf123b
Jonelle Villar <jonellevillar@gmail.com>
2022-03-26 17:36:26 +0100

    extra slides

--- extraSlides.R
+++ extraSlides.R
@@ -6,3 +6,20 @@ class:top left
   - Diagnosis & health status <br> <br />
   - Time of day & season <br> <br />
   
+  
+  
+  class:top left
+### Results <br> <br> Genes associated with CpGs at genome-wide significance
+
+.enlarge[
+  - GLT8D1: Glycosyltransferase 8 Domain Containing 1 <br> <br />
+    - SPCS1: signal peptidase complex subunit 1 <br> <br />
+    - LOC100507065:long noncoding RNA <br> <br />
+]
+
+
+
+
+width="500px" height="500px"
+style="position:absolute; right:5px; top:130px;">
+  
